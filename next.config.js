0a66e9ccab728f4ce9195872e52f2bbf9413f8b7
mateja <mateja176@gmail.com>
2020-09-16 21:03:23 +0200

    adds env variables to next.config

--- next.config.js
+++ next.config.js
@@ -8,4 +8,14 @@ module.exports = {
       },
     ];
   },
+  env: {
+    firebaseApiKey: 'AIzaSyBJpWY3zxbrQw8omGdZ5ls_nT3gq1pvo4E',
+    firebaseAuthDomain: 'learn-ed.firebaseapp.com',
+    firebaseDatabaseUrl: 'https://learn-ed.firebaseio.com',
+    firebaseProjectId: 'learn-ed',
+    firebaseStorageBucket: 'learn-ed.appspot.com',
+    firebaseMessagingSenderId: '555673042379',
+    firebaseAppId: '1:555673042379:web:2ea7ff082d549feaa5ca4e',
+    firebaseMeasurementId: 'G-6B32XEYKB',
+  },
 };
