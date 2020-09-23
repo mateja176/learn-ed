ef2a1591e2c4a989e1bb4896b6485a2a6f816f7e
mateja <mateja176@gmail.com>
2020-09-23 21:54:00 +0200

    makes Breadcrumbs responsive

--- components/Breadcrumbs/Breadcrumbs.tsx
+++ components/Breadcrumbs/Breadcrumbs.tsx
@@ -8,30 +8,33 @@ export interface BreadcrumbsProps {
   pathname: string;
 }
 
+const navStyle: React.CSSProperties = {
+  display: 'flex',
+  flexWrap: 'wrap',
+};
+
 const Breadcrumbs: React.FC<BreadcrumbsProps> = (props) => {
   const segments = props.pathname.slice(1).split('/');
   return (
-    <nav>
-      <Text color="gray">
-        {segments.map((segment, i) => {
-          const isLast = i === segments.length - 1;
-          const formattedSegment = startCase(segment);
-          return (
-            <span key={segment.concat(i.toString())}>
-              &nbsp;
-              {isLast ? (
-                formattedSegment
-              ) : (
-                <Link href={urljoin('/', ...segments.slice(0, i + 1))}>
-                  {formattedSegment}
-                </Link>
-              )}
-              &nbsp;
-              {!isLast && '/'}
-            </span>
-          );
-        })}
-      </Text>
+    <nav style={navStyle}>
+      {segments.map((segment, i) => {
+        const isLast = i === segments.length - 1;
+        const formattedSegment = startCase(segment);
+        return (
+          <Text color="gray" key={segment.concat(i.toString())}>
+            {isLast ? (
+              formattedSegment
+            ) : (
+              <Link href={urljoin('/', ...segments.slice(0, i + 1))}>
+                {formattedSegment}
+              </Link>
+            )}
+            &nbsp;
+            {!isLast && '/'}
+            &nbsp;
+          </Text>
+        );
+      })}
     </nav>
   );
 };
