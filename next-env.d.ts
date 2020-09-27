05b8118ad4f767e91031d0aa09af31bb4f849103
mateja <mateja176@gmail.com>
2020-09-27 19:37:36 +0200

    adds svg import declaration

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
+import { FC, SVGProps } from 'react';
+
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.svg' {
+  const SVGComponent: FC<SVGProps<SVGSVGElement>>;
+  export default SVGComponent;
+}
