15d0aee2c528bebf0bdcc4d197bac71ed2673394
mateja <mateja176@gmail.com>
2020-10-10 00:44:52 +0200

    extracts global.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,2 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module '*.svg' {
-  import * as React from 'react';
-
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-
-  export default ReactComponent;
-}
