eecce2e36cf31d040c3bf136b0fc2d02dfb53c12
mateja <mateja176@gmail.com>
2020-10-12 01:43:26 +0200

    uses image-size

--- scripts/convertSvgAssets.ts
+++ scripts/convertSvgAssets.ts
@@ -1,14 +1,19 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
 import * as fs from 'fs';
+import { imageSize } from 'image-size';
 import { join } from 'path';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import * as svgexport from 'svgexport'; // eslint-disable-line import/no-extraneous-dependencies
+import * as svgexport from 'svgexport';
 
 const assetsPath = join(__dirname, '..', 'assets');
 
 fs.readdir(assetsPath, { encoding: 'utf-8' }, (err, paths) => {
   if (!err) {
     paths.forEach((path, i) => {
+      const { width, height } = imageSize(join('assets', path));
+
       const data = {
         input: join(assetsPath, path),
         output: `${join(
@@ -17,7 +22,7 @@ fs.readdir(assetsPath, { encoding: 'utf-8' }, (err, paths) => {
           'public',
           'img',
           path.replace(/svg$/, 'png'),
-        )} 192:`,
+        )} ${width && height ? (width > height ? '192:' : ':192') : '192:192'}`,
       };
 
       // * without the timeout a significant amount of images are skipped
