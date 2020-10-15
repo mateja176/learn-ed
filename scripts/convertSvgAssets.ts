a9bf8571c0255b9d3332beec3b43583b070da05c
mateja <mateja176@gmail.com>
2020-10-16 01:22:04 +0200

    pads pngs

--- scripts/convertSvgAssets.ts
+++ scripts/convertSvgAssets.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 import * as fs from 'fs';
-import { imageSize } from 'image-size';
 import { join } from 'path';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
@@ -12,17 +11,17 @@ const assetsPath = join(__dirname, '..', 'assets');
 fs.readdir(assetsPath, { encoding: 'utf-8' }, (err, paths) => {
   if (!err) {
     paths.forEach((path, i) => {
-      const { width, height } = imageSize(join('assets', path));
-
+      const input = join(assetsPath, path);
+      const output = join(
+        __dirname,
+        '..',
+        'public',
+        'img',
+        path.replace(/svg$/, 'png'),
+      );
       const data = {
-        input: join(assetsPath, path),
-        output: `${join(
-          __dirname,
-          '..',
-          'public',
-          'img',
-          path.replace(/svg$/, 'png'),
-        )} ${width && height ? (width > height ? '200:' : ':200') : '200:200'}`,
+        input,
+        output: `${output} pad 200:200`,
       };
 
       // * without the timeout a significant amount of images are skipped
